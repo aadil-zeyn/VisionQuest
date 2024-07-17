8c57808cde1155a56181cd6de3c2c8140fa6bfb1
Aadil <zeyngaming49@gmail.com>
2024-07-17 22:13:44 +0530

    refactor: student -> user

--- models/student.js
+++ models/student.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const studentSchema = new mongoose.Schema({
   name: String,
   email: String,
+  score: Number,
   topics: [String],
   achievements: [String],
 }, { versionKey: false });
