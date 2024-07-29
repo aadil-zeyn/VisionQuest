5f8142751c146ca8c8aca5cb151a2e9e192cdb1c
Aadil <aadhu1857@gmail.com>
2024-07-30 00:59:53 +0530

    fix: update challenge model

--- models/challenge.js
+++ models/challenge.js
@@ -1,8 +1,17 @@
 import { Schema, model } from 'mongoose';
 
+const testcaseSchema = new Schema({
+  input: String,
+  expectedOutput: String
+})
+
 const challengeSchema = new Schema({
   title: String,
   description: String,
+  boilerplateCode: String,
+  visibleTestCases: [testcaseSchema],
+
+  hiddenTestCases: [testcaseSchema],
   topic: String,
 }, { versionKey: false });
 
